9513fb326659fa312764c6f2230282df5f1003f1
sunjiaaa <117729181+sunjiaaa@users.noreply.github.com>
2022-11-08 19:38:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小汪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oGVft6qVEfeOKnDgvlNHT0y1pSNk',
+      id: 'oGVft6vqzzbOZdayZz8CcTATI0xM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '9P9s4oBkczUFJsUd2UtEzUsVIIRGvY2dNJwKWkOe4fg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
